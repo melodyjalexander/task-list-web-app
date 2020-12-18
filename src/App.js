cb97832d6c82b9f75c041a1fe159607fe43a12fa
melodyjalexander <melodyjalexander@gmail.com>
2020-12-18 18:29:55 -0500

    Update React app with three text variables

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>I am a React Application!</div>;
+  let myTitle = "Today";
+  let taskStatus = "Upcoming";
+  let sampleTask = "Pick up drying cleaning";
+  return (
+    <div>
+      <h1>{myTitle}</h1>
+      <h2>{taskStatus}</h2>
+      <ul>
+        <li>{sampleTask}</li>
+        <li>{sampleTask}</li>
+        <li>{sampleTask}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
