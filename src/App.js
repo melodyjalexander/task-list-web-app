4e424c76b25a0e4b693bbe6ceabf219d3b9b8396
melodyjalexander <melodyjalexander@gmail.com>
2021-01-21 02:31:19 -0500

    Task list renders via JSX function, objects created for user and task data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./App.css";
+import avatarImage from "./sampleAvatar.jpeg";
 
 function App() {
-  let avatarImage = require("./sampleAvatar.jpeg");
+  const user = {
+    firstName: "Lucy",
+    lastName: "Young",
+    avatar: avatarImage,
+  };
+
+  function formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+
+  const todoList = [
+    {
+      id: 1,
+      title: "Business",
+      description: "Batch package orders from Saturday",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Pet",
+      description: "Pick up dog food at 2pm",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Health",
+      description: "Schedule teeth cleaning soon",
+      isCompleted: false,
+    },
+  ];
+
   return (
     <div>
       <header>
@@ -16,7 +47,7 @@ function App() {
             className="userAvatar"
             alt="sample avatar illustration"
           ></img>
-          <h2>Lucy Young</h2>
+          <h2>{formatName(user)}</h2>
         </div>
       </header>
 
@@ -36,16 +67,24 @@ function App() {
 
       <div className="taskList">
         <p className="taskListTitle">Current</p>
-        <div className="taskContainer">
-          <label>
-            <input type="radio" name="radio-button" value="task" />
-            <span>Package batch orders from Saturday</span>
-          </label>
-          <button className="editButton">+</button>
-        </div>
+
+        {todoList.map((todoItem) => (
+          <div className="taskContainer">
+            <h5 className="taskTag">{todoItem.title}</h5>
+            <div className="taskActionsContainer">
+              <label>
+                <input type="radio" name="radio-button" value="{isCompleted}" />
+                <span>{todoItem.description}</span>
+              </label>
+              <button className="editButton">+</button>
+            </div>
+          </div>
+        ))}
 
         <div>
-          <button className="addTask">Add new task</button>
+          <button className="addTask">
+            <h5>Add new task</h5>
+          </button>
         </div>
       </div>
     </div>
